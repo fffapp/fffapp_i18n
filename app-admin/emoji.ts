46f49cbd069eec5d046bcd094af8715094fb2fd5
fffapp <info@fffapp.org>
2019-10-12 12:33:27 +0200

    new schema

---
+++ app-admin/emoji.ts
@@ -0,0 +1,85 @@
+export const emojiI18N = {
+  it : { 
+    search: 'Cerca',
+    emojilist: 'Lista emoji',
+    notfound: 'Nessun emoji trovato',
+    clear: 'Pulisci',
+    categories: {
+      search: 'Risultati ricerca',
+      recent: 'Usati recentemente',
+      people: 'Faccine & Gente',
+      nature: 'Animali & Natura',
+      foods: 'Cibo & Bevande',
+      activity: 'Attività',
+      places: 'Viaggi & Luoghi',
+      objects: 'Oggetti',
+      symbols: 'Simboli',
+      flags: 'Bandiere',
+      custom: 'Personali'
+    },
+    skintones: {
+      1: 'Default Skin Tone',
+      2: 'Light Skin Tone',
+      3: 'Medium-Light Skin Tone',
+      4: 'Medium Skin Tone',
+      5: 'Medium-Dark Skin Tone',
+      6: 'Dark Skin Tone',
+    }
+  },
+  en: {
+    search: 'Search',
+    emojilist: 'List of emoji',
+    notfound: 'No Emoji Found',
+    clear: 'Clear',
+    categories: {
+      search: 'Search Results',
+      recent: 'Frequently Used',
+      people: 'Smileys & People',
+      nature: 'Animals & Nature',
+      foods: 'Food & Drink',
+      activity: 'Activity',
+      places: 'Travel & Places',
+      objects: 'Objects',
+      symbols: 'Symbols',
+      flags: 'Flags',
+      custom: 'Custom',
+    },
+    skintones: {
+      1: 'Default Skin Tone',
+      2: 'Light Skin Tone',
+      3: 'Medium-Light Skin Tone',
+      4: 'Medium Skin Tone',
+      5: 'Medium-Dark Skin Tone',
+      6: 'Dark Skin Tone',
+    }
+  },
+  pl: {
+    search: 'Search',
+    emojilist: 'List of emoji',
+    notfound: 'No Emoji Found',
+    clear: 'Clear',
+    categories: {
+      search: 'Search Results',
+      recent: 'Frequently Used',
+      people: 'Smileys & People',
+      nature: 'Animals & Nature',
+      foods: 'Food & Drink',
+      activity: 'Activity',
+      places: 'Travel & Places',
+      objects: 'Objects',
+      symbols: 'Symbols',
+      flags: 'Flags',
+      custom: 'Custom',
+    },
+    skintones: {
+      1: 'Default Skin Tone',
+      2: 'Light Skin Tone',
+      3: 'Medium-Light Skin Tone',
+      4: 'Medium Skin Tone',
+      5: 'Medium-Dark Skin Tone',
+      6: 'Dark Skin Tone',
+    }
+  }
+
+
+}
